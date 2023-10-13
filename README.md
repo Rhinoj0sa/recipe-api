eccd2cd4e595e924a7fdd340a01cfb92a9a29f54
Rhinoj0sa <rhinojosaram@gmail.com>
2023-10-13 02:52:48 -0600

    message

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 course
 follow the course
 generate the token dckr_pat_o46ouEEOs5wi_0c8VuK0Q6iH6m0
+
+after creating a new app named core
+wee remove test and views.py because this server will be a rest API.
--- app/app/calc.py
+++ app/app/calc.py
@@ -11,3 +11,13 @@ def add(x, y):
 def subtract(x, y):
     """Subtract y from x and return result."""
     return x - y
+
+values = {'a': 1, 'b': 2, 'c': 3}
+def side_effect(arg):
+    return values[arg]
+
+mock.side_effect = side_effect
+mock('a'), mock('b'), mock('c')
+
+mock.side_effect = [5, 4, 3, 2, 1]
+mock(), mock(), mock()
