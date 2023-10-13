d2c2f384c2ee910b148f57be0b697273b7051630
Rhinoj0sa <rhinojosaram@gmail.com>
2023-10-13 03:14:18 -0600

    remove unwanted code

--- app/app/calc.py
+++ app/app/calc.py
@@ -16,8 +16,3 @@ values = {'a': 1, 'b': 2, 'c': 3}
 def side_effect(arg):
     return values[arg]
 
-mock.side_effect = side_effect
-mock('a'), mock('b'), mock('c')
-
-mock.side_effect = [5, 4, 3, 2, 1]
-mock(), mock(), mock()
