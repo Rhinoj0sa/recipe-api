22ca7237d7dbba6c27c4343670d77422dce4d9b9
Rhinoj0sa <rhinojosaram@gmail.com>
2023-10-13 03:19:04 -0600

    linter corrections

--- app/app/calc.py
+++ app/app/calc.py
@@ -12,7 +12,9 @@ def subtract(x, y):
     """Subtract y from x and return result."""
     return x - y
 
-values = {'a': 1, 'b': 2, 'c': 3}
+
+values = {"a": 1, "b": 2, "c": 3}
+
+
 def side_effect(arg):
     return values[arg]
-
