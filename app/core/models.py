d161333014a8d418b9aafd415da74b02ccf0385c
Rhinoj0sa <rhinojosaram@gmail.com>
2023-10-13 19:25:59 -0600

    corrected user model

--- app/core/models.py
+++ app/core/models.py
@@ -14,15 +14,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
+
+    def normalize_email(self, email):
+        """Normalize email address."""
+        return email.lower()
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -30,4 +34,4 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
